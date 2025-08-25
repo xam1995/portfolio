e67cfd4b73075e7be0832b67793ad15bcec01e97
xam1995 <glacial.saga.0z@icloud.com>
2025-08-26 00:38:28 +0200

    Seperated server and client.

--- app/projects/[slug]/page.tsx
+++ app/projects/[slug]/page.tsx
@@ -1,45 +1,12 @@
-"use client"
-
-import {Container, Grid, Typography} from "@mui/material";
-import {redirect, useParams} from "next/navigation";
 import {getProjectBySlug, getProjects} from "@/services/projects/service";
-import {Project, Technology} from "@/data/model/types";
-import {Stack} from "@mui/system";
-import GitHubButton from "@/components/ui/projects/buttons/GitHubButton";
-import DemoButton from "@/components/ui/projects/buttons/DemoButton";
-import ArticleButton from "@/components/ui/projects/buttons/ArticleButton";
-import {TechCard} from "@/components/ui/cv/TechCard/TechCard";
+import ProjectPageClient from "@/components/pages/projects/Project";
 
 export async function generateStaticParams() {
     const slugs = getProjects().map(proj => proj.slug);
-    return slugs.map((slug) => ({ slug }));
+    return slugs.map((slug) => ({slug}));
 }
 
-export default function ProjectPage() {
-    const {slug} = useParams();
-    const project: Project = getProjectBySlug(slug);
-
-    if (!project) { redirect("/")}
-
-    return (
-        <Container sx={{paddingY: 5, minHeight: "90vh"}}>
-            <Grid container spacing={2} direction="column">
-                <Typography variant="h3">{project.title}</Typography>
-                {project.images && (
-                    <Grid container spacing={2} paddingY={2} style={{display: "flex"}}>
-                        <img src={`../${project.images[0]}`} alt={`${project.slug}1`}
-                             style={{flex: 1, display: "flex", maxWidth:"100%", maxHeight: 300, objectFit: "cover"}}/>
-                    </Grid>)}
-                <Grid container spacing={2}>
-                    {project.technologies?.map((technology: Technology) => (<TechCard key={technology.name} technology={technology}/>))}
-                </Grid>
-                <Typography flex={1} variant="subtitle1" textAlign="justify">{project.summary}</Typography>
-                <Stack direction="row" spacing={2}>
-                    <DemoButton videoLink={project.videoLink}/>
-                    <GitHubButton repoLink={project.repoLink}/>
-                    <ArticleButton articleLink={project.articleLink}/>
-                </Stack>
-            </Grid>
-        </Container>
-    );
+export default async function ProjectPage({params}: { params: { slug: string } }) {
+    const project = await getProjectBySlug(params.slug);
+    return (<ProjectPageClient project={project}/>);
 }
---
+++ components/pages/projects/Project.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+
+import {Project, Technology} from "@/data/model/types";
+import {Container, Grid, Stack} from "@mui/system";
+import {redirect} from "next/navigation";
+import {Typography} from "@mui/material";
+import {TechCard} from "@/components/ui/cv/TechCard/TechCard";
+import DemoButton from "@/components/ui/projects/buttons/DemoButton";
+import GitHubButton from "@/components/ui/projects/buttons/GitHubButton";
+import ArticleButton from "@/components/ui/projects/buttons/ArticleButton";
+
+export default function ProjectPageClient({ project }: { param: Project }) {
+    if (!project) { redirect("/")}
+
+    return (
+        <Container sx={{paddingY: 5, minHeight: "90vh"}}>
+            <Grid container spacing={2} direction="column">
+                <Typography variant="h3">{project.title}</Typography>
+                {project.images && (
+                    <Grid container spacing={2} paddingY={2} style={{display: "flex"}}>
+                        <img src={`../${project.images[0]}`} alt={`${project.slug}1`}
+                             style={{flex: 1, display: "flex", maxWidth:"100%", maxHeight: 300, objectFit: "cover"}}/>
+                    </Grid>)}
+                <Grid container spacing={2}>
+                    {project.technologies?.map((technology: Technology) => (<TechCard key={technology.name} technology={technology}/>))}
+                </Grid>
+                <Typography flex={1} variant="subtitle1" textAlign="justify">{project.summary}</Typography>
+                <Stack direction="row" spacing={2}>
+                    <DemoButton videoLink={project.videoLink}/>
+                    <GitHubButton repoLink={project.repoLink}/>
+                    <ArticleButton articleLink={project.articleLink}/>
+                </Stack>
+            </Grid>
+        </Container>
+    );
+}
