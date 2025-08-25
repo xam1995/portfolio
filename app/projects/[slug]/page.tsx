e33294459dd9ada74e18aae5c72a66b41ca80d10
xam1995 <glacial.saga.0z@icloud.com>
2025-08-26 00:41:35 +0200

    Fixed promise.

--- app/projects/[slug]/page.tsx
+++ app/projects/[slug]/page.tsx
@@ -6,7 +6,7 @@ export async function generateStaticParams() {
     return slugs.map((slug) => ({slug}));
 }
 
-export default async function ProjectPage({params}: { params: { slug: string } }) {
+export default async function ProjectPage({params}: { params: { slug: Promise<string> } }) {
     const project = await getProjectBySlug(params.slug);
     return (<ProjectPageClient project={project}/>);
 }
