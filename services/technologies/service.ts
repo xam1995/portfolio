48c750a437d25a7ad7aa8e506b0fb6430316e09f
xam1995 <glacial.saga.0z@icloud.com>
2025-08-29 17:01:49 +0200

    Fixed technology service after refactoring.

--- services/technologies/service.ts
+++ services/technologies/service.ts
@@ -14,23 +14,3 @@ export function getTechnology(name: string): Technology {
 }
 
 
-export function getLanguages(): Technology[] {
-    return TECHNOLOGIES.filter((tech: Technology) => tech.isLanguage);
-}
-
-
-export function getFrameworks(): Technology[] {
-    return TECHNOLOGIES.filter((tech: Technology) => tech.isFramework);
-}
-
-
-export function getTools(): Technology[] {
-    return TECHNOLOGIES.filter((tech: Technology) => tech.isTool);
-}
-
-
-export function getLibraries(): Technology[] {
-    return TECHNOLOGIES.filter((tech: Technology) => tech.isLibrary);
-}
-
-
